593cb5f36719c02ca4cfcf677a597d346136123b
Denis Strelkov <strelkov.d.d@gmail.com>
2022-04-25 20:50:35 +0300

    wip

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,46 +3,34 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const path = require("path");
 
-const demoDir = path.join(__dirname, "demo");
-const outputDir = path.join(demoDir, "bundle");
+const buildDir = path.join(__dirname, "build");
+const outputDir = path.join(buildDir, "bundle");
 // const publicDir = path.join(demoDir, "public");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const pages = [
-  {
-    title: "Index",
-    slug: "index",
-    entry: "./demo/Index.bs.js",
-    path_: ".",
-  },
-  {
-    title: "Page 1",
-    slug: "page1",
-    entry: "./demo/Page1.bs.js",
-    path_: "page1",
-  },
-  {
-    title: "Page 2",
-    slug: "page2",
-    entry: "./demo/Page2.bs.js",
-    path_: "page2",
-  },
-];
+const pages = require("./build/pages.json");
+console.log(pages);
+// title
+// slug
+// entryPath
+// outputDir
+// htmlTemplatePath
 
 const entries = Object.fromEntries(
-  pages.map(({ slug, entry }) => [slug, entry])
+  pages.map(({ slug, entryPath }) => [slug, entryPath])
 );
 
 const htmlWebpackPlugins = pages.map(
-  ({ title, slug, path_ }) =>
+  ({ title, slug, outputDir, htmlTemplatePath }) =>
     new HtmlWebpackPlugin({
       title: title,
       lang: "en",
       // path to html template
-      template: path.join(demoDir, "index-static.html"),
-      // output filename
-      filename: path.join(`${path_}/index.html`),
+      template: htmlTemplatePath,
+      // relative path to page?
+      filename:
+        slug === "index" ? "./index.html" : path.join(`${slug}/index.html`),
       chunks: [slug],
       inject: true,
       // minify html
@@ -67,21 +55,24 @@ module.exports = {
 
   plugins: [new CleanWebpackPlugin(), ...htmlWebpackPlugins],
 
-  optimization: {
-    splitChunks: {
-      // include all types of chunks
-      chunks: "all",
-      // Minimum size, in bytes, for a chunk to be generated.
-      minSize: 100,
-    },
-  },
+  // optimization: {
+  //   splitChunks: {
+  //     // include all types of chunks
+  //     chunks: "all",
+  //     // Minimum size, in bytes, for a chunk to be generated.
+  //     minSize: 10000,
+  //   },
+  // },
 
   devServer: {
     historyApiFallback: true,
+    hot: false,
     // static: {
     //   directory: path.join(__dirname, "public"),
     // },
     // compress: true,
     port: 9007,
   },
+
+  // stats: 'verbose'
 };
